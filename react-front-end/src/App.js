94cd09b5e572668d8f112a622594269e607f4535
hseiji <henrique.hst@gmail.com>
2022-07-29 08:21:28 -0400

    fix npm versions for leaflet

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -3,9 +3,23 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Homepage from "./components/Homepage/Homepage";
 import Navbar from "./components/Navbar/Navbar";
+import { MapContainer, Marker, Popup, TileLayer, useMap } from 'react-leaflet'
 
 function App() {
   return (
+
+    // <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+    //   <TileLayer
+    //     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    //     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    //   />
+    //   <Marker position={[51.505, -0.09]}>
+    //     <Popup>
+    //       A pretty CSS3 popup. <br /> Easily customizable.
+    //     </Popup>
+    //   </Marker>
+    // </MapContainer>
+
     <div className="App">
       <Routes>
         <Route
@@ -28,5 +42,7 @@ function App() {
   //   </div>
   // )
 }
+
+
 
 export default App;
