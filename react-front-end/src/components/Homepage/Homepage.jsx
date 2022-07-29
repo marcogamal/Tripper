5d220e2036c64b51c6c8b1a3869dc8b7c7447ccb
Marco <marco.salman@gmail.com>
2022-07-28 22:19:00 -0400

    adding homepage

---
+++ react-front-end/src/components/Homepage/Homepage.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import "./Homepage.css";
+
+export default function Homepage() {
+  return (
+    <div className="wrapper" id="map">
+      <div className="list">LIST OF PLACES HERE</div>
+
+      <div className="map">MAP GOES HERE</div>
+    </div>
+  );
+}
