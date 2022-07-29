5d18c5c3a58446b8d76003085d699ecefcbf6086
hseiji <henrique.hst@gmail.com>
2022-07-29 18:10:33 -0400

    add ERD to images folder

--- react-front-end/src/components/Map/Map.jsx
+++ react-front-end/src/components/Map/Map.jsx
@@ -5,7 +5,7 @@ export const Map = () => {
 
 
   return (
-    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
