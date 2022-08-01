9db3bd9dc2682ecd01d0e6bbc7c963bd12c37555
Marco <marco.salman@gmail.com>
2022-07-31 23:46:07 -0400

    putting logo on navbar

--- react-front-end/src/components/Navbar/Navbar.jsx
+++ react-front-end/src/components/Navbar/Navbar.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import "./Navbar.css";
 
+const logo = require('./TripperLogo.png'); 
+
 export default function Navbar() {
   return (
     <div className="navbar">
-      Navbar
+      <div className="navbarLogo" href="#">
+      <img src={logo} width="400" height="150" alt=""></img>
+      </div>
+      <div className="SignIn">
+
+      </div>
     </div>
   );
 }
