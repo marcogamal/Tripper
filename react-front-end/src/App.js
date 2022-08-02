703d889a6c5e65dc09f4f1f082fcfc08e9b6b32d
Marco <marco.salman@gmail.com>
2022-08-01 22:22:25 -0400

    refactoring app to include its children

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -1,10 +1,21 @@
 import "./App.css";
-import React from "react";
+import React, { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import Homepage from "./components/Homepage/Homepage";
 import Navbar from "./components/Navbar/Navbar";
 
 function App() {
+const [result2, setResult2] = useState([])
+const events = [
+  {
+    latitude: 43.64446719365264,
+    longitude: -79.38649706503828,
+  },
+  {
+    latitude: 43.64220060887206,
+    longitude: -79.3864107609249,
+  }
+];
   return (
 
     <div className="App">
@@ -13,8 +24,8 @@ function App() {
           path="/"
           element={
             <>
-              <Navbar />
-              <Homepage />
+              <Navbar events={events} results={result2} setResults={setResult2}/>
+              <Homepage events={events} results={result2}/> 
             </>
           }
         />
