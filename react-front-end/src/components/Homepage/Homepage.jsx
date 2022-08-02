d2a4817026b947bd2ef8666105fa025d9ea1c1d8
hseiji <henrique.hst@gmail.com>
2022-08-01 22:46:20 -0400

    add coords to map

--- react-front-end/src/components/Homepage/Homepage.jsx
+++ react-front-end/src/components/Homepage/Homepage.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Map } from "../Map/Map";
 import { SearchBar } from "../SearchBar/SearchBar";
 import { addToMap } from "../../helpers/MapFunctions";
@@ -6,7 +6,7 @@ import "./Homepage.css";
 
 export default function Homepage() {
 
-  const events = [
+  const [events, setEvents] = useState([
     {
       latitude: 43.64446719365264,
       longitude: -79.38649706503828,
@@ -14,13 +14,24 @@ export default function Homepage() {
     {
       latitude: 43.64220060887206,
       longitude: -79.3864107609249,
-    }
-  ];
+    }    
+  ]);
+
+  // const events = [
+  //   {
+  //     latitude: 43.64446719365264,
+  //     longitude: -79.38649706503828,
+  //   },
+  //   {
+  //     latitude: 43.64220060887206,
+  //     longitude: -79.3864107609249,
+  //   }  
+  // ]
 
   return (
     <div className="wrapper">
-      <SearchBar className="list" addFunc={addToMap} events={events}/>
-      <Map events={events}/>
+      <SearchBar className="list" addFunc={setEvents} events={events}/>
+      <Map markers={events} addMarkers={setEvents}/>
     </div>
   );
 }
--- react-front-end/src/components/ListItem/ListItem.jsx
+++ react-front-end/src/components/ListItem/ListItem.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 
 export const ListItem = (props) => {
 
   //TODO: include button to Add event to map (save to DB)
+
+  // Add marker
+  const addMarker = () => {
+    const newMarker = {
+      latitude: props.latitude,
+      longitude: props.longitude,
+    };
+    props.addFunc((prevMarkers) => [...prevMarkers, newMarker]);
+    console.log("adding event");
+  };
 
   return (
     <li>
@@ -10,7 +20,8 @@ export const ListItem = (props) => {
         <>
           <p>{props.name}</p>
           <p>Rating: {props.rating} ({props.review} reviews)</p>
-          <button onClick={props.addFunc(props.latitude, props.longitude, props.events)}>Add</button>
+          {/* <button onClick={() => props.addFunc(props.latitude, props.longitude, props.events)}>Add</button> */}
+          <button onClick={addMarker}>Add</button>
         </>      
       }
     </li>
