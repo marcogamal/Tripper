dec885903498c2a8678b1347ef1681a13cbc8208
hseiji <henrique.hst@gmail.com>
2022-08-01 23:12:21 -0400

    Revert "refactoring app to include its children"
    This reverts commit 703d889a6c5e65dc09f4f1f082fcfc08e9b6b32d.

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -1,21 +1,10 @@
 import "./App.css";
-import React, { useState } from "react";
+import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Homepage from "./components/Homepage/Homepage";
 import Navbar from "./components/Navbar/Navbar";
 
 function App() {
-const [result2, setResult2] = useState([])
-const events = [
-  {
-    latitude: 43.64446719365264,
-    longitude: -79.38649706503828,
-  },
-  {
-    latitude: 43.64220060887206,
-    longitude: -79.3864107609249,
-  }
-];
   return (
 
     <div className="App">
@@ -24,8 +13,8 @@ const events = [
           path="/"
           element={
             <>
-              <Navbar events={events} results={result2} setResults={setResult2}/>
-              <Homepage events={events} results={result2}/> 
+              <Navbar />
+              <Homepage />
             </>
           }
         />
