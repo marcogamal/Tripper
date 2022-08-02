aa9259c88a39f91c7efe8dd5f85240bbca68e8ec
hseiji <henrique.hst@gmail.com>
2022-08-01 23:10:46 -0400

    Revert "refactoring / fixing the search bar"
    This reverts commit 55875ff6407a7a02cbe74d6bdb3e64c15774aff6.

--- react-front-end/src/components/SearchBar/SearchBar.jsx
+++ react-front-end/src/components/SearchBar/SearchBar.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from 'react'
 import axios from 'axios'
-import "./SearchBar.css"
-
+import "./SearchBar"
+import { ResultList } from '../ResultList/ResultList';
 
 export const SearchBar = (props) => {
-const {setResults} = props
+
   const [keyword, setKeyword] = useState("");
-// const [results, setResults] = useState([]);
+  const [results, setResults] = useState([]);
   const [location, setLocation] = useState("");
 
   const resultsSet = (data) => {
@@ -23,8 +23,7 @@ const {setResults} = props
       infoAll.push(infoItem);
     });
     console.log("infoAll", infoAll);
-//    setResults([])
-   setResults(infoAll)
+    setResults(prevState => infoAll)
   }
 
   const handleSubmit = async (e) => {
@@ -32,55 +31,31 @@ const {setResults} = props
 
     try {
       const resp = await axios.get(`http://localhost:8080/api/events/${keyword}/${location}`);
-      console.log("this is resp", resp )
       resultsSet(resp.data)
-//      console.log("results:", results);
+      console.log("results:", results);
     } catch (error) {
       console.log(error);
     }
 
   }
 
-//   const handleChangeKeyword = (e) => {
-//     setKeyword(e.target.value)
-//   }
+  const handleChangeKeyword = (e) => {
+    setKeyword(e.target.value)
+  }
 
-//   const handleChangeLocation = (e) => {
-//     setLocation(e.target.value)
-//   }
-
-//   return (
-//     <div className='searchbar'>
-//       <h2>Search</h2>
-//       <form onSubmit={handleSubmit}>
-//         <input type="text" placeholder="type here ..." id="keyword" value={keyword} onChange={(e) => handleChangeKeyword(e)}></input>
-//         <input type="text" placeholder="type here ..." id="location" value={location} onChange={(e) => handleChangeLocation(e)}></input>
-//         <button>Search</button>
-
-//       </form>
-//     </div>
-//   )
-// }
-
-  const handleChange = (e) => {
-    const {name, value} = e.target
-    console.log("name", name)
-    if (name === "keyword") {
-      setKeyword(value)
-    } 
-    else if (name === "location"){
-      setLocation(value)
-    } 
+  const handleChangeLocation = (e) => {
+    setLocation(e.target.value)
   }
 
   return (
     <div className='searchbar'>
       <h2>Search</h2>
       <form onSubmit={handleSubmit}>
-        <input name="keyword" type="text" placeholder="type here ..." id="keyword" value={keyword} onChange={handleChange}></input>
-        <input name="location" type="text" placeholder="type here ..." id="location" value={location} onChange={handleChange}></input>
+        <input type="text" placeholder="type here ..." id="keyword" value={keyword} onChange={(e) => handleChangeKeyword(e)}></input>
+        <input type="text" placeholder="type here ..." id="location" value={location} onChange={(e) => handleChangeLocation(e)}></input>
         <button>Search</button>
-
+        <h3>Results:</h3>
+        <ResultList results={results} addFunc={props.addFunc} events={props.events}/>
       </form>
     </div>
   )
