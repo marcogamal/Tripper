1e5a0c76bb13f37cc25e3315f90636e163622eb8
Marco <marco.salman@gmail.com>
2022-08-06 21:44:21 -0400

    adding the profile page for storing events

---
+++ react-front-end/src/components/Profile/Profile.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export const Profile = () => {
+  return (
+    <div className="wrapper-profile">
+      <h2>My Plans</h2>
+      <div className="profile">
+        <p> Plan 1</p>
+        <ul>
+          ROM,
+          CN Tower
+
+        </ul>
+      </div>
+    </div>
+  );
+};
