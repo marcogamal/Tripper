773e439e1eefe9f7edc385df579c49ad34024d34
ConsensusAI <malik.iqbal@hotmail.ca>
2022-08-07 20:09:49 -0400

    Add map layer initialization back

--- react-front-end/src/components/MapLayer/MapLayer.jsx
+++ react-front-end/src/components/MapLayer/MapLayer.jsx
@@ -1,13 +1,12 @@
-
-import React, { useContext, useEffect } from 'react'
-import { LayerGroup, Marker, Popup, useMap } from 'react-leaflet';
+import React, { useContext, useEffect } from "react";
+import { LayerGroup, Marker, Popup, useMap } from "react-leaflet";
 import * as L from "leaflet";
-import { AppContext } from '../hooks/useAppContext';
-import MapRouting from '../MapRouting';
+import { AppContext } from "../hooks/useAppContext";
+import MapRouting from "../MapRouting";
 
 export const MapLayer = () => {
-  
-  const { events, deleteFromMap, changeIconColor, showRoutes } = useContext(AppContext);
+  const { events, deleteFromMap, changeIconColor, showRoutes } =
+    useContext(AppContext);
   const LeafIcon = L.Icon.extend({
     options: {},
   });
@@ -20,6 +19,8 @@ export const MapLayer = () => {
         "https://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|2ecc71&chf=a,s,ee00FFFF",
     });
 
+  const map = useMap();
+
   useEffect(() => {
     console.log("rendering map", events);
   }, [events]);
@@ -29,22 +30,25 @@ export const MapLayer = () => {
       {events.map((ele) => {
         return (
           <div key={ele.id}>
-              <Marker 
+            <Marker
               key={ele.id}
               position={[ele.latitude, ele.longitude]}
-              icon={ele.done ? greenIcon : blueIcon}>
-                <Popup>
-                  {ele.name}
-                  <br/>
-                  <br/>
-                  <button onClick={() => deleteFromMap(ele.id)}>Delete</button>
-                  <button onClick={() => changeIconColor(ele.id)}>{ele.done ? <>Uncheck</> : <>Done</>}</button>
-                </Popup>
-              </Marker>             
+              icon={ele.done ? greenIcon : blueIcon}
+            >
+              <Popup>
+                {ele.name}
+                <br />
+                <br />
+                <button onClick={() => deleteFromMap(ele.id)}>Delete</button>
+                <button onClick={() => changeIconColor(ele.id)}>
+                  {ele.done ? <>Uncheck</> : <>Done</>}
+                </button>
+              </Popup>
+            </Marker>
           </div>
         );
       })}
-      {showRoutes && <MapRouting map={map}/>}
+      {showRoutes && <MapRouting map={map} />}
     </LayerGroup>
   );
 };
